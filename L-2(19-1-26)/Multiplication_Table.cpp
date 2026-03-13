5250ab1ca5c42e0c82c6e4a6433377ecd2bb9f24
Misthy Sonalkar <misthysonalkar@gmail.com>
2026-03-13 13:04:36 +0530

    Add multiplication table program in C++

---
+++ L-2(19-1-26)/Multiplication_Table.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cout<<"Enter the value of a:"<<endl;
+    cin>>a;
+    for(int i = 1; i <= 10; i++)
+        cout << "7 x " << i << " = " << a*i << endl;
+
+    return 0;
+}
