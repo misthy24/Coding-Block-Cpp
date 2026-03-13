e2bbd96eecad294e14df60fe586169490eed69e4
Misthy Sonalkar <misthysonalkar@gmail.com>
2026-03-13 11:05:31 +0530

    Create a C++ program to add two numbers
    Implement a simple program to add two numbers.

---
+++ L-2(19-1-26)/Add two numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter the value:"<<endl;
+    cin>>a>>b;
+    cout<<"Addition of two values:"<<a+b;
+    return 0;
+}
