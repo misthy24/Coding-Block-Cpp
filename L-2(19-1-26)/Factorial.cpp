dcf6e1808c97c792b08804811c0f06574b44d44a
Misthy Sonalkar <misthysonalkar@gmail.com>
2026-03-13 11:13:07 +0530

    Add Factorial program in C++

---
+++ L-2(19-1-26)/Factorial.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    long long fact = 1;
+    cout << "Enter number: ";
+    cin >> n;
+
+    for(int i = 1; i <= n; i++)
+        fact = fact * i;
+
+    cout << "Factorial of Number= " << fact;
+    return 0;
+}
